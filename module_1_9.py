57f4363e5194874ffc42dedb83fc5b35e0e94413
SweatyKitty <nigensey@gmail.com>
2024-10-27 20:02:56 +0300

    Update module_1_9.py

---
+++ module_1_9.py
@@ -0,0 +1,8 @@
+example='Unrealistic'  #Task1
+print(example[0])#Task2
+print(example[-1])#Task3
+print(example[5:])#Task4
+print(example[len(example)//2:])#Task4.1:Попробовал не совсем по предложенному алгоритму
+print(example[::-1])#Task5
+print(example[1::2])#Task6
+

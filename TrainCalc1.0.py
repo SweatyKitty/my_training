54807b520a461d1acb24f46f05a27a50f621d98e
SweatyKitty <nigensey@gmail.com>
2024-11-21 19:07:27 +0300

    update calculator credit

--- TrainCalc1.0.py
+++ TrainCalc1.0.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 def month_3():
-    count(1)
+    count(1,3)
 def month_12():
-    count(2)
+    count(2,12)
 def month_18():
-    count(3)
+    count(3,18)
 def month_36():
-    count(4)
+    count(4,24)
 
-def count(i=1):
+def count(i=1,f=3):
     a=int(number1_entry.get())
     b=6*i
     if i==4:
@@ -18,31 +18,69 @@ def count(i=1):
     c=100-bb
     d=a/c
     l=round(d*100,2)
+    z=round(l/f,2)
     answer_entry.delete(0, 'end')
     answer_entry.insert(0, l )
+    answer_2_entry.delete(0,'end')
+    answer_2_entry.insert(0, z)
 
+def month_payment(i=1):
+    a=float(answer_entry.get())
+    b=round(a/i,2)
+    answer_2_entry.delete(0, 'end')
+    answer_2_entry.insert(0, b)
+def dop_month_6():
+    month_payment(6)
+def dop_month_10():
+    month_payment(10)
+def dop_month_36():
+    month_payment(36)
 
 window=tk.Tk()
 window.title("Калькулятор суммы для рассрочки")
 window.geometry('600x450')
 window.resizable(False,False)
 
-button_3_month=tk.Button(window,width=12, height=3, text='3-10 м.', command=month_3)
-button_3_month.place(x=24, y=250)
+button3_title=tk.Label(window, text='Платеж рассчитан по первой цифре диапазона месяцев \n При необходимости уточните месяцы:')
+button3_title.place(x=24, y=195)
 
-button_12_month=tk.Button(window,width=12, height=3, text='12-18',command=month_12)
-button_12_month.place(x=173, y=250)
+button_3_month=tk.Button(window,width=12, height=3, text='от 3х до 10 \n месяцев', command=month_3)
+button_3_month.place(x=24, y=230)
 
-button_18_month=tk.Button(window,width=12, height=3, text='18-24',command=month_18)
-button_18_month.place(x=322, y=250)
+button_6_month=tk.Button(window,width=5, height=1, text='6м.', command=dop_month_6)
+button_6_month.place(x=24, y=290)
+button_10_month=tk.Button(window,width=5, height=1, text='10м.', command=dop_month_10)
+button_10_month.place(x=73, y=290)
 
-button_36_month=tk.Button(window,width=12, height=3, text='24-36',command=month_36)
-button_36_month.place(x=471, y=250)
+button_12_month=tk.Button(window,width=12, height=5, text='от 12 до 18\n месяцев',command=month_12)
+button_12_month.place(x=173, y=230)
+
+button_18_month=tk.Button(window,width=12, height=5, text='от 18 до 24\n месяцев',command=month_18)
+button_18_month.place(x=322, y=230)
+
+button_36_month=tk.Button(window,width=12, height=3, text='от 24 до 36\n месяцев',command=month_36)
+button_36_month.place(x=471, y=230)
+
+button_36_2_month=tk.Button(window,width=12, height=1, text='36м.', command=dop_month_36)
+button_36_2_month.place(x=471, y=290)
+
+number1_title=tk.Label(window, text='Введите сколько должен получить магазин:')
+number1_title.place(x=24, y=160)
+
 
 number1_entry=tk.Entry(window, width=90)
-number1_entry.place(x=24, y=200)
+number1_entry.place(x=24, y=180)
+
+number2_title=tk.Label(window, text='Сумма для указания на сайте с рассрочкой:')
+number2_title.place(x=24, y=320)
 
 answer_entry=tk.Entry(window, width=90)
 answer_entry.place(x=24, y=340)
+
+number3_title=tk.Label(window, text='Примерный платеж в месяц')
+number3_title.place(x=24, y=380)
+
+answer_2_entry=tk.Entry(window, width=90)
+answer_2_entry.place(x=24, y=400)
 
 window.mainloop()
--- module_3_hard.py
+++ module_3_hard.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 from tkinter import Button, Entry
+def gen_check():
+    if i is list:
+        list_check(u)
+    elif i is dict:
+        dict_check(u)
+    elif i is tuple:
+        tuple_check()
+    elif i is set:
+        set_check()
+    else:
+        int_float_String_bool_check()
 
-def list_check():
+def list_check(u):
 
-def dict_check():
+def dict_check(u):
+    a=[*u]
+    z=0
+    for i in range(len(u)):
+        z=z+len(a[i])+int(u[a[i]])+1
+    return(z)
 
-def tuple_check():
 
-def set_check():
+def tuple_check(u):
+
+def set_check(u):
 
 def int_float_String_bool_check():
 
 def count():
     data_structure=[string_Entry.get()]
     for i in data_structure:
-
+        isinstance()
 
 window=tk.Tk()
 window.title('Окно для задачи хард 3 модуля')
